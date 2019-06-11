e5c1bb1b77c1564c5efa521cb00909025c53bc68
Ayoub Mammeri <ay.mammeri@gmail.com>
2019-06-11 14:27:04 -0400

    turn on loader animation the first time

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 const imageContainer = document.getElementById('image-container');
 const loader = document.getElementById('loader');
 
-let ready = false;
 let imagesLoaded = 0;
-let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
@@ -11,9 +9,11 @@ const count = 10;
 const apiKey = 'a4MmsZ3MWen7GjhmSB6LliOwUTVnu1WhvxDRsRyPY7I';
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Loaded images counter 
 function imageLoaded() {
     imagesLoaded++
 }
+
 // function to set attributes to an element 
 function setAttributes(element, attributes) {
     for (const key in attributes) {
@@ -45,6 +45,8 @@ function displayPhotos() {
         item.appendChild(img);
         imageContainer.appendChild(item);
     });
+    // hide the loader after the loading images 
+    loader.hidden = true;
 }
 
 
@@ -65,6 +67,7 @@ async function getPhotos(){
 window.addEventListener('scroll', () => {
     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && imagesLoaded === photosArray.length) {
         imagesLoaded = 0;
+
         getPhotos();
     }
 });
